b24cd7dc45ed5e8b98c13223e9d489add4d77c3a
Nathan Lardizabal <nate@njil.dev>
2024-05-10 14:52:45 -0400

    fix: bypass 403 with user agent

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const getEpisodes = async () => {
 
   try {
     const response = await fetch(
-      'https://webscraper.io/test-sites/e-commerce/allinone'
+      'https://www.crunchyroll.com/series/GYZJ43JMR/that-time-i-got-reincarnated-as-a-slime',
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          userAgent:
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+        },
+      }
     )
-
     const text = await response.text()
+
     const $ = cheerio.load(text)
 
-    $(
-      '.wrapper > .container > .row > .col-lg-9 > .row > .col-md-4 > .card > .product-wrapper'
-    ).each((index, element) => {
-      let title = $(element).find('.caption > h4:nth-child(2) > a').text()
-
-      episodeData.push({
-        title,
-      })
+    $('#content > div > div > div:nth-child(2)').each((index, element) => {
+      console.log(element)
     })
   } catch (error) {
     throw error
   }
-
-  console.log(episodeData)
 }
 
 getEpisodes()
