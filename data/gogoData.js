87aae6458327067b48583968ccbb57d48b47eb05
Nathan Lardizabal <nate@njil.dev>
2024-05-10 21:01:02 -0400

    feat: :sparkles: using `lowdb` for db

---
+++ data/gogoData.js
@@ -0,0 +1,3 @@
+export const gogoData = [
+  'https://gogoanime.gg/category/yoru-no-kurage-wa-oyogenai',
+]
--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 import express from 'express'
 import { gogo } from './lib/gogo.js'
+import { getDB } from './lib/db.js'
+import { gogoData } from './data/gogoData.js'
 
 const app = new express()
+const db = await getDB()
 
-app.get('/scrape', async (req, res, next) => {
-  console.log('scraping')
+app.get('/gogo', async (req, res, next) => {
+  console.log('checking for new episodes...')
+  const episodes = await gogo(gogoData[0])
 
-  const eps = await gogo(
-    'https://gogoanime.gg/category/yoru-no-kurage-wa-oyogenai'
-  )
+  const { eps } = await db.data
 
-  console.log(eps)
+  const ep = {
+    title: episodes[0].title,
+    latestEp: episodes[0].href,
+    epCount: episodes.length,
+  }
 
-  res.json(eps)
+  // if it's not in the db, add it
+  if (eps.filter((e) => e.title === ep.title).length === 0) {
+    console.log(`adding ${ep.title} to db 🎉`)
+    await db.update(({ eps }) => eps.push(ep))
+  }
+  // if there's a new episode
+  else if (eps[0].epCount !== ep.epCount) {
+    console.log(`new episode found! watch here: ${ep.latestEp}`)
+    await db.update(({ eps }) => eps && (eps[0] = ep))
+  } else console.log('no new episodes 🥲')
+
+  return res.json(episodes)
 })
 
-app.listen(2093, () => console.log('running on port 2093'))
+app.listen(2093, () => console.log(`running on http://localhost:2093/gogo`))
